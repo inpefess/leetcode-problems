ca417fdc1269fe79f1591aa65fcd86de76fa2d44
Boris Shminke <boris.shminke.ml>
2019-12-14 19:25:45 +0300

    solution to the length of longest substring problem

---
+++ leetcode_problems/length_of_longest_substring.py
@@ -0,0 +1,49 @@
+"""
+    leetcode-problems is a collection of Python 3 solutions for problems
+    from https://leetcode.com/
+    Copyright (C) 2019  Boris Shminke
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from typing import Dict
+
+
+class Solution:
+    """ Can be copied and pasted to LeetCode site. """
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        Given a string, find the length of the longest substring without
+        repeating characters.
+
+        :param s: input string
+        :returns: the length of the optimal substring
+        """
+        str_len = len(s)
+        substr: Dict[str, int] = dict()
+        max_len = 0
+        start_index = 0
+        for i in range(str_len):
+            duplicate_index = substr.get(s[i], -1)
+            if duplicate_index != -1:
+                current_len = i - start_index
+                if current_len > max_len:
+                    max_len = current_len
+                for j in range(start_index, duplicate_index + 1):
+                    del substr[s[j]]
+                start_index = duplicate_index + 1
+            substr[s[i]] = i
+        current_len = str_len - start_index
+        if current_len > max_len:
+            max_len = current_len
+        return max_len
---
+++ tests/test_length_of_longest_substring.py
@@ -0,0 +1,37 @@
+"""
+    leetcode-problems is a collection of Python 3 solutions for problems
+    from https://leetcode.com/
+    Copyright (C) 2019  Boris Shminke
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from unittest import TestCase
+
+from parameterized import parameterized
+
+from leetcode_problems.length_of_longest_substring import Solution
+
+
+class TestLengthOfLongestSubstring(TestCase):
+    @parameterized.expand([
+        ("abcabcbb", 3),
+        ("bbbbb", 1),
+        ("pwwkew", 3),
+        (" ", 1)
+    ])
+    def test_solution(self, substring, length):
+        self.assertEqual(
+            Solution().lengthOfLongestSubstring(substring),
+            length
+        )
